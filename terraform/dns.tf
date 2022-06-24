665a81c27338f398a407b1d18f8019f81eb7d006
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-24 21:11:04 +0200

    Fix incorrect DNS record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "assets" {
   zone_id = data.terraform_remote_state.cloudsetup.outputs.mdekort_zone_id
-  name    = "mdekort-assets"
+  name    = "assets"
   type    = "CNAME"
   ttl     = 1
   proxied = true
