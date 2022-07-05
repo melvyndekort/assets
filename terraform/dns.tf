aca654a3dbbdf5f6b43b8488c9c57cc98ce8abdd
Melvyn de Kort <melvyn@mdekort.nl>
2022-07-05 11:49:21 +0200

    More generic Terraform setup

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,8 +1,8 @@
-resource "cloudflare_record" "assets" {
+resource "cloudflare_record" "site" {
   zone_id = data.terraform_remote_state.cloudsetup.outputs.mdekort_zone_id
-  name    = "assets"
+  name    = var.domain_name
   type    = "CNAME"
   ttl     = 1
   proxied = false
-  value   = "mdekort-assets.netlify.app"
+  value   = "${var.name}.netlify.app"
 }
