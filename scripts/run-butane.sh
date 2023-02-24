00bc93ea72e4f84526ff2c4727f9b08611ddd0ec
Melvyn de Kort <melvyn@mdekort.nl>
2023-02-24 22:15:32 +0100

    Moved ignition stuff to their own repo

--- scripts/run-butane.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for FILE in butane-cfgs/*.bu; do
-  IGN="$(basename $FILE | sed 's/.bu$/.ign/')"
-  docker run --interactive --rm --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release --pretty --strict $FILE > src/ignition/$IGN
-done
-
