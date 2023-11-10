33a1d9bc436376f18304a6fe7502774523884760
Melvyn de Kort <melvyn@mdekort.nl>
2023-11-10 16:18:13 +0100

    Migrate state from Terraform Cloud to S3

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,10 @@
 terraform {
   required_version = "~> 1.5.0"
 
-  cloud {
-    organization = "melvyndekort"
-
-    workspaces {
-      name = "assets"
-    }
+  backend "s3" {
+    bucket = "mdekort.tfstate"
+    key    = "assets.tfstate"
+    region = "eu-west-1"
   }
 
   required_providers {
